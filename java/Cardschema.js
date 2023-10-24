a6487d960ea1e15f031bddb733b690b8d5691d77
ManoSCherian <147057337+ManoSCherian@users.noreply.github.com>
2023-10-24 11:36:40 +0530

    UST425 Create Cardschema.js

---
+++ java/Cardschema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const cardSchema = mongoose.Schema({
+    email: String,
+    pinCode: String
+})
+
+
+
+module.exports = mongoose.model('cowin', cardSchema, false);
+
